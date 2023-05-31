36aff07053dee54be5d84628d992804fee0df30c
JSanLazaro <jsanlazaro@hotmail.com>
2023-05-31 15:40:15 +0200

    positioning item elements

--- src/js/cloud/cloud.js
+++ src/js/cloud/cloud.js
@@ -11,20 +11,38 @@ class NameCloud {
     const self = this;
 
     const elCloud = document.createElement("div");
-    elCloud.className = "cloud";
+    elCloud.className = "cloudEl";
+    elCloud.style.position = 'relative';
 
-    self.texts.forEach((text) => {
+    self.texts.forEach((text,index) => {
       const item = self.createTextItem(text);
+      position = computePosition(index);
+      item.style.left = position.x;
+      item.style.top = position.y;
       elCloud.appendChild(item);
     });
     self.container.appendChild(elCloud);
   }
   createTextItem(text) {
     const itemEl = document.createElement("span");
-    itemEl.className = "item";
+    itemEl.className = "itemEl";
+    itemEl.style.position = 'absolute';
     itemEl.innerText = text;
     return itemEl;
   }
+  computePosition(index, random = false) {
+    const self = this;
+    const textsLength = self.texts.length;
+    // if random `true`, It means that a random appropriate place is generated, and the position will be independent of `index`
+    if (random) index = Math.floor(Math.random() * (textsLength + 1));
+    const phi = Math.acos(-1 + (2 * index + 1) / textsLength);
+    const theta = Math.sqrt((textsLength + 1) * Math.PI) * phi;
+    return {
+        x: (self.size * Math.cos(theta) * Math.sin(phi)) / 2,
+        y: (self.size * Math.sin(theta) * Math.sin(phi)) / 2,
+        z: (self.size * Math.cos(phi)) / 2,
+    };
+}
 }
 createCloud();
 function createCloud() {
