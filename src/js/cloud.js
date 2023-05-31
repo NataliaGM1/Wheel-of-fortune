f85df264b79f23d1375c1684b769fd8bedc79ca6
TxemaBlanco <sam@example.comtxemabl@hotmail.com>
2023-05-31 14:49:58 +0200

    add items to cloud

--- src/js/cloud.js
+++ src/js/cloud.js
@@ -1,24 +1,58 @@
-createCloud();
-function createCloud(){
-    const names=getNames();
-    const container=getCloudElement();
-    addItemsTo(names,container);
+class NameCloud {
+  constructor(container, texts) {
+    alert("dentro del constructor");
+    const self = this;
+    self.container = container;
+    self.texts = texts;
+    self.createInnerCloud();
+  }
+  createInnerCloud() {
+    const self = this;
+
+    const elCloud = document.createElement("div");
+    elCloud.className = "cloud";
+
+    self.texts.forEach((text) => {
+      const item = self.createTextItem(text);
+      elCloud.appendChild(item);
+    });
+    self.container.appendChild(elCloud);
+  }
+  createTextItem(text) {
+    const itemEl = document.createElement("span");
+    itemEl.className = "item";
+    itemEl.innerText = text;
+    return itemEl;
+  }
 }
-function getNames(){
-    const names=[
-    'Juan','Carlos','Miguel','Jesus','Pedro','Michael','Pablo','Mateo','Santi'    
-    ]
-    return names;
-    
+createCloud();
+function createCloud() {
+  const textNames = getNames();
+  const cloudContainer = getCloudElement();
+  const namesCloud = new NameCloud(cloudContainer, textNames);
+}
+function getNames() {
+  const names = [
+    "Juan",
+    "Carlos",
+    "Miguel",
+    "Jesus",
+    "Pedro",
+    "Michael",
+    "Pablo",
+    "Mateo",
+    "Santi",
+  ];
+  return names;
 }
 
-function getCloudElement(){
-    const element=document.getElementById("cloud");
-    return element;
+function getCloudElement() {
+  const element = document.getElementById("cloud");
+  return element;
 }
-function addItemsTo(items,cloud){
-    for(let item of items){
-    const node=document.createElement("div"); 
-    cloud.appendChild(node); 
-    }
+function addItemsTo(items, cloud) {
+  for (let item of items) {
+    const node = document.createElement("div");
+    cloud.appendChild(node);
+  }
 }
