7159f4805b554133a9a156c12b1181f5538373af
maesga1 <artemys201@gmail.com>
2023-06-05 13:55:06 +0200

    Alert in process

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,3 +1,13 @@
-function mostrarAlerta() {
-  alert("¡Te ha tocado la pelota estas Eliminado!");
-}
+//    function mostrar(){
+
+//   swal({
+//    text: 'TE HA TOCADO LA PELOTA ESTÁS ELIMINADO:',   icon: 'error',  
+// //   } );
+
+//  }
+// When the user clicks on div, open the popup
+  function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+                            }
+
