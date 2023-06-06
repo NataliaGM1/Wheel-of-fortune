14435df3db582ff190b0f932e651eaa3715bfabf
Natalia Mihalache <ngmihalache@gmail.com>
2023-06-06 15:52:56 +0200

    pop-up Eliminado

--- src/js/glass.js
+++ src/js/glass.js
@@ -1,12 +1,36 @@
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
+
   
-searchButton.addEventListener('click', function() {
-  const searchTerm = searchInput.value;
-  // Realiza la lógica de búsqueda con el término ingresado
-  console.log('Buscando: ' + searchTerm);
-}); 
-  
-  
+  // Obtener el botón para abrir el pop-up y el pop-up mismo
+const openButton = document.getElementById("openPopup");
+const popup = document.getElementById("popup");
+const closeButton = document.getElementsByClassName("close")[0];
+
+// Función para mostrar el pop-up
+function showPopup() {
+  popup.style.display = "block";
+}
+
+// Función para ocultar el pop-up
+function hidePopup() {
+  popup.style.display = "none";
+}
+
+// Agregar evento de clic al botón para abrir el pop-up
+openButton.addEventListener("click", showPopup);
+
+// Agregar evento de clic al botón de cierre y al área fuera del pop-up para ocultarlo
+closeButton.addEventListener("click", hidePopup);
+window.addEventListener("click", function (event) {
+  if (event.target == popup) {
+    hidePopup();
+  }
+});
+
+
+
+
+
+
+
   
   
